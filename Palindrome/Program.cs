c09b0281fef8a6e472cfc405f1435f9e0671bb31
Austin <austincumberlander@gmail.com>
2019-03-09 11:16:58 -0600

    done

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Palindrome
 {
@@ -6,7 +7,35 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void palindrome(string input)
+            {
+                char[] array = input.ToCharArray();
+                Array.Reverse(array);
+                string reversed = new string(array);
+                var result = input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+
+                if (result == true)
+                {
+                    Console.WriteLine($"{input} is a palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is not a palindrome!");
+                }
+                //List<char> theInput = new List<char>();
+
+                //foreach(char character in input)
+                //{
+                //    theInput.Add(character);
+                //    Console.WriteLine(character);
+                //}
+
+                //Console.WriteLine("theInput: ", theInput);
+            }
+
+            Console.WriteLine($"Enter word to check if it's a palindrome: ");
+            palindrome(Console.ReadLine());
+            Console.ReadLine();
         }
     }
 }
